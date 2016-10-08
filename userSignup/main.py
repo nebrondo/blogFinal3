d34d288b24b0770b8b62612116eccff0d946c1cf
415000 <camendoza@DIRECTV.com>
2016-10-08 08:34:13 -0700

    initial commit

--- userSignup/main.py
+++ userSignup/main.py
@@ -45,31 +45,40 @@ class MainHandler(TemplateHandler):
         self.render("index.html")
         #self.response.out.write(form)
     def post(self):
-        user = self.request.get("user")
-        pass = self.request.get("pass")
-        c_pass = self.request.get("c_pass")
+        user = self.request.get("username")
+        pwd = self.request.get("password")
+        c_pass = self.request.get("verify")
         email = self.request.get("email")
+        userWarning = passWarning = c_passWarning = ""
 
         if not user:
             userWarning = "Empty username"
-        if not pass:
+        if not pwd:
             passWarning = "Empty password"
         if not c_pass:
             c_passWarning = "Confirm password"
-        elif pass != c_pass:
+        elif pwd != c_pass:
             passWarning = "Password do not match"
 
 
         if not re.match(r"[^@]+@[^@]+\.[^@]+",email):
             emailWarning = "Invalid email"
 
-        if userWarning or passWarning or c_passWarning or emailWarning
-            self.render("index.html",user=user)
+        if userWarning or passWarning or c_passWarning:
+            self.render("index.html",user=user,email=email,userWarning=userWarning,passWarning=passWarning,c_passWarning=c_passWarning)
+        else:
+            userCookie = ("username=%s; Path=/welcome" % user).encode('ascii','ignore')
+            self.response.headers.add_header("Set-Cookie", userCookie)
+
+            self.redirect("/welcome")
         #self.redirect("/rot13")
-
-
+class WelcomeHandler(TemplateHandler):
+    def get(self):
+        username=self.request.cookies.get("username")
+        self.render("welcome.html",username=username)
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler),
-    ('/register',MainHandler)
+    ('/signup',MainHandler),
+    ('/welcome',WelcomeHandler)
 ], debug=True)
